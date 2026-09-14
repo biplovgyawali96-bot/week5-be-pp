da3d65fe74c6ff016594f1a5d6d3cbac858bd6d6
Pratham Arora <prathamarora9f15@gmail.com>
2026-09-14 14:27:03 +0300

    refactor: migrate tours API to MongoDB

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
-require('dotenv').config();
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+
+const connectDB = require("./config/db");
+
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
 const { unknownEndpoint } = require("./middleware/customMiddleware");
 
 const morgan = require("morgan");
+
 app.use(morgan("dev"));
 
 // Middleware to parse JSON
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running');
+app.get("/", (req, res) => {
+  res.send("API is running");
 });
-
 
 // Use the tourRouter for all "/tours" routes
 app.use("/api/tours", tourRouter);
 
-// Use the userRouter for all /users routes
+// Use the userRouter for all "/users" routes
 app.use("/api/users", userRouter);
 
 app.use(unknownEndpoint);
-// app.use(errorHandler);
 
 const port = process.env.PORT || 4000;
+
+// Connect to MongoDB
+connectDB();
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
- 
